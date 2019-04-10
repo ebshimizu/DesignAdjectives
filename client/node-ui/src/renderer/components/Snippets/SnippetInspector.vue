d6f0aae6b2e21be64a9ea3a575e81d17a31438c1
Evan Shimizu <falindrith@gmail.com>
2019-04-10 15:53:43 -0400

    adding snippet training info

--- client/node-ui/src/renderer/components/Snippets/SnippetInspector.vue
+++ client/node-ui/src/renderer/components/Snippets/SnippetInspector.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="flex flex-row w-full h-full flex-wrap overflow-auto items-start">
-    <exemplar
-      v-for="(ex, idx) in localExemplarList"
-      :key="activeSnippet + idx"
-      v-bind:snippet-name="activeSnippet"
-      v-bind:id="idx"
-    ></exemplar>
+  <div class="flex flex-row h-full w-full overflow-hidden">
+    <div class="w-64 border-r border-grey-lightest font-sans overflow-auto text-grey-lightest">
+      <div class="border-b border-blue-darkest bg-blue-lighter text-blue-dark px-2 py-1 text-sm">
+        <div class="font-bold">{{ activeSnippet !== '' ? activeSnippet : '[No Active Snippet]' }}</div>
+        <div class="my-1">{{ status }}</div>
+      </div>
+    </div>
+    <div class="flex flex-row w-full h-full flex-wrap overflow-auto items-start">
+      <exemplar
+        v-for="(ex, idx) in localExemplarList"
+        :key="activeSnippet + idx"
+        v-bind:snippet-name="activeSnippet"
+        v-bind:id="idx"
+      ></exemplar>
+    </div>
   </div>
 </template>
 
@@ -26,12 +34,38 @@ export default {
     activeSnippet() {
       return this.$store.state.snippets.activeSnippet;
     },
-    exemplarList() {
+    activeSnippetObject() {
       if (this.activeSnippet in this.$store.state.snippets.snippets) {
-        return this.$store.state.snippets.snippets[this.activeSnippet].data;
+        return this.$store.state.snippets.snippets[this.activeSnippet];
+      }
+
+      return null;
+    },
+    exemplarList() {
+      if (this.activeSnippetObject) {
+        return this.activeSnippetObject.data;
       } else {
         return [];
       }
+    },
+    status() {
+      if (this.activeSnippetObject) {
+        return Object.keys(this.activeSnippetObject.trainData).length > 0
+          ? 'Trained'
+          : 'Not Trained';
+      }
+    },
+    trainData() {
+      const filtered = {};
+      if (this.activeSnippetObject) {
+        for (key of Object.keys(this.activeSnippetObject.trainData)) {
+          if (key !== 'code' && key !== 'message') {
+            filtered[key] = this.activeSnippetObject.trainData[key];
+          }
+        }
+      }
+
+      return filtered;
     }
   },
   watch: {
