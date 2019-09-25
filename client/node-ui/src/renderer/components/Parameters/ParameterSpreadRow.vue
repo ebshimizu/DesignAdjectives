f06c30c5509ee7a5d31733726d273e20fab93326
Evan Shimizu <ebshimizu@gmail.com>
2019-09-25 11:41:10 -0400

    resolve #72
    initial implementation complete

--- client/node-ui/src/renderer/components/Parameters/ParameterSpreadRow.vue
+++ client/node-ui/src/renderer/components/Parameters/ParameterSpreadRow.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="w-full flex flex-row items-center">
-    <div class="w-1/5 px-2 text-gray-200 font-bold text-md font-mono tracking-wide">{{ name }}</div>
+  <div class="w-full flex flex-row items-center p-1" :class="[activeBG]">
+    <div class="w-1/5 flex flex-col items-center">
+      <div
+        class="w-full h-full px-2 text-gray-200 font-bold text-md font-mono tracking-wide text-center py-3 break-all"
+      >[{{id}}] {{ name }}</div>
+      <div class="green square-button uppercase tracking-wide" @click="toggleActive">Select</div>
+    </div>
     <spread-sample
       v-for="item in spread"
       :key="item.label"
@@ -14,6 +19,7 @@
 
 <script>
 import SpreadSample from '../Samples/SpeadSample';
+import { MUTATION } from '../../store/constants';
 
 export default {
   name: 'parameter-spread-row',
@@ -44,6 +50,17 @@ export default {
       }
 
       return data;
+    },
+    activeBG() {
+      return this.active ? 'bg-gray-700' : '';
+    }
+  },
+  methods: {
+    toggleActive() {
+      this.$store.commit(MUTATION.CHANGE_PARAM_ACTIVE, {
+        id: this.id,
+        active: !this.active
+      });
     }
   }
 };
